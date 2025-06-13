e6d660d1032caf311e1b21c37d63fb9231529469
Gitika Kalal <gitikakalal21@gmail.com>
2025-06-13 14:30:46 -0700

    completed  ChatEntry component with sender, message, and timestamp display and connect messages.json in App to render first message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css';
+import DATA from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
+
 
 const App = () => {
+  const firstMessagesData = DATA[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <div>
+          <ChatEntry 
+            sender={firstMessagesData.sender}
+            body={firstMessagesData.body}
+            timeStamp={firstMessagesData.timeStamp}
+          ></ChatEntry>
+        </div>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
